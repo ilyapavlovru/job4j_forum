c225e9ee72b497836489422c8d78e1cd68730594
Ilya Pavlov <ilya.pavlov@list.ru>
2021-09-01 11:29:46 +0300

    create job4j_forum

---
+++ src/test/java/ru/job4j/forum/ForumApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.job4j.forum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForumApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
