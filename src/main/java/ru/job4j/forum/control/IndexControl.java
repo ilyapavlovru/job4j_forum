7af1b2ded9379f19ffec986a51568b06e67f3453
Ilya Pavlov <ilya.pavlov@list.ru>
2021-09-03 22:48:42 +0300

    2. Spring boot security [#296071] - Рефакторинг и чистка кода

--- src/main/java/ru/job4j/forum/control/IndexControl.java
+++ src/main/java/ru/job4j/forum/control/IndexControl.java
@@ -4,31 +4,19 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import ru.job4j.forum.model.Comment;
-import ru.job4j.forum.model.Post;
-import ru.job4j.forum.model.User;
-import ru.job4j.forum.service.CommentService;
-import ru.job4j.forum.service.PostService;
-import ru.job4j.forum.service.UserService;
 
-import javax.servlet.http.HttpServletRequest;
+import ru.job4j.forum.model.Post;
+import ru.job4j.forum.service.PostService;
+
 import java.util.Collection;
-import java.util.Optional;
 
 @Controller
 public class IndexControl {
 
     private final PostService postService;
-    private final CommentService commentService;
-    private final UserService userService;
 
-    public IndexControl(PostService postService, CommentService commentService, UserService userService) {
+    public IndexControl(PostService postService) {
         this.postService = postService;
-        this.commentService = commentService;
-        this.userService = userService;
     }
 
     @GetMapping({"/", "/index"})
@@ -37,58 +25,5 @@ public class IndexControl {
         Collection<Post> posts = postService.findAllPosts();
         model.addAttribute("posts", posts);
         return "index";
-    }
-
-    @GetMapping("/create")
-    public String create(Model model) {
-        model.addAttribute("user", SecurityContextHolder.getContext().getAuthentication().getPrincipal());
-        return "post/create";
-    }
-
-    @PostMapping("/save")
-    public String save(@ModelAttribute Post post, HttpServletRequest req) {
-        String userName = SecurityContextHolder.getContext().getAuthentication().getName();
-        User user = userService.findByUserName(userName);
-        post.setUser(user);
-        postService.savePost(post);
-        return "redirect:/";
-    }
-
-    @GetMapping("/update")
-    public String update(@RequestParam("id") int id, Model model) {
-        model.addAttribute("user", SecurityContextHolder.getContext().getAuthentication().getPrincipal());
-        Optional<Post> post = postService.findPostById(id);
-        post.ifPresent(value -> model.addAttribute("post", value));
-        return "post/update";
-    }
-
-    @GetMapping("/show")
-    public String show(@RequestParam("id") int id, Model model) {
-        model.addAttribute("user", SecurityContextHolder.getContext().getAuthentication().getPrincipal());
-        Optional<Post> post = postService.findPostById(id);
-        post.ifPresent(value -> model.addAttribute("post", value));
-        return "post";
-    }
-
-    @GetMapping("/addComment")
-    public String addComment(@RequestParam("id") int id, Model model) {
-        Optional<Post> post = postService.findPostById(id);
-        post.ifPresent(value -> model.addAttribute("post", value));
-        return "comment/create";
-    }
-
-    @PostMapping("/saveComment")
-    public String save(@RequestParam("id") int id, @ModelAttribute Comment comment, HttpServletRequest req, Model model) {
-        model.addAttribute("user", SecurityContextHolder.getContext().getAuthentication().getPrincipal());
-        String userName = SecurityContextHolder.getContext().getAuthentication().getName();
-        User user = userService.findByUserName(userName);
-        Post post = postService.findPostById(id).get();
-        Comment createdComment = new Comment();
-        createdComment.setText(comment.getText());
-        createdComment.setUser(user);
-        createdComment.setPost(post);
-        commentService.saveComment(createdComment);
-        model.addAttribute("post", post);
-        return "post";
     }
 }
---
+++ src/main/java/ru/job4j/forum/control/PostControl.java
@@ -0,0 +1,84 @@
+package ru.job4j.forum.control;
+
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import ru.job4j.forum.model.Comment;
+import ru.job4j.forum.model.Post;
+import ru.job4j.forum.model.User;
+import ru.job4j.forum.service.CommentService;
+import ru.job4j.forum.service.PostService;
+import ru.job4j.forum.service.UserService;
+
+import java.util.Optional;
+
+@Controller
+public class PostControl {
+
+    private final UserService userService;
+    private final PostService postService;
+    private final CommentService commentService;
+
+    public PostControl(UserService userService, PostService postService, CommentService commentService) {
+        this.userService = userService;
+        this.postService = postService;
+        this.commentService = commentService;
+    }
+
+    @GetMapping("/create")
+    public String create(Model model) {
+        model.addAttribute("user", SecurityContextHolder.getContext().getAuthentication().getPrincipal());
+        return "post/create";
+    }
+
+    @PostMapping("/save")
+    public String save(@ModelAttribute Post post) {
+        String userName = SecurityContextHolder.getContext().getAuthentication().getName();
+        User user = userService.findByUserName(userName);
+        post.setUser(user);
+        postService.savePost(post);
+        return "redirect:/";
+    }
+
+    @GetMapping("/update")
+    public String update(@RequestParam("id") int id, Model model) {
+        model.addAttribute("user", SecurityContextHolder.getContext().getAuthentication().getPrincipal());
+        Optional<Post> post = postService.findPostById(id);
+        post.ifPresent(value -> model.addAttribute("post", value));
+        return "post/update";
+    }
+
+    @GetMapping("/show")
+    public String show(@RequestParam("id") int id, Model model) {
+        model.addAttribute("user", SecurityContextHolder.getContext().getAuthentication().getPrincipal());
+        Optional<Post> post = postService.findPostById(id);
+        post.ifPresent(value -> model.addAttribute("post", value));
+        return "post";
+    }
+
+    @GetMapping("/addComment")
+    public String addComment(@RequestParam("id") int id, Model model) {
+        Optional<Post> post = postService.findPostById(id);
+        post.ifPresent(value -> model.addAttribute("post", value));
+        return "comment/create";
+    }
+
+    @PostMapping("/saveComment")
+    public String saveComment(@RequestParam("id") int id, @ModelAttribute Comment comment, Model model) {
+        model.addAttribute("user", SecurityContextHolder.getContext().getAuthentication().getPrincipal());
+        String userName = SecurityContextHolder.getContext().getAuthentication().getName();
+        User user = userService.findByUserName(userName);
+        Post post = postService.findPostById(id).get();
+        Comment createdComment = new Comment();
+        createdComment.setText(comment.getText());
+        createdComment.setUser(user);
+        createdComment.setPost(post);
+        commentService.saveComment(createdComment);
+        model.addAttribute("post", post);
+        return "post";
+    }
+}
